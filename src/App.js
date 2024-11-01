273d8511c73c61fab661611b92ceda10ddc0c595
Kishan <kishanrajrby2@gmail.com>
2024-11-02 02:51:49 +0530

    Prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [color, SetColor] = useState("brown")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container-fluid d-flex bottom-1 justify-content-center align-items-end min-vh-100' style={{ backgroundColor: color }}>
+        <div className='row'>
+          <div className='col-lg-5 text-center box-1'>
+            <button className='btn btn-primary m-1 bluebtn-1' onClick={() => SetColor("blue")}>Blue</button>
+            <button className='btn btn-primary m-1 bluebtn-2' onClick={() => SetColor("green")}>Green</button>
+            <button className='btn btn-primary m-1 bluebtn-3' onClick={() => SetColor("yellow")}>Yellow</button>
+            <button className='btn btn-primary m-1 bluebtn-4' onClick={() => SetColor("black")}>Black</button>
+            <button className='btn btn-primary m-1  bluebtn-5' onClick={() => SetColor("grey")}>Gray</button>
+          </div>
+        </div>
+      </div>
+
+
+    </>
   );
 }
 
---
+++ src/Headingcmp.js
@@ -0,0 +1,8 @@
+function Headingcmp(){
+    return(
+        <>
+            <h1 className="d-flex justify-content-center">Kishan Raj</h1>            
+        </>
+    );
+}
+export default Headingcmp;
